31faadc9ed552dafe1484a10b2892c6dde4cf286
gitairman <aaronhopkins86@gmail.com>
2024-01-08 17:24:02 -0800

    1.7: unicafe step 2

--- Exercises1.6.-1.14./unicafe/src/App.jsx
+++ Exercises1.6.-1.14./unicafe/src/App.jsx
@@ -5,6 +5,9 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const all = good + neutral + bad
+  const average = (good*1 + neutral*0 + bad*-1) / all || 0
+  const percentGood = (good / all) * 100 || 0
 
   const handleClickGood = () => {
     setGood(good + 1)
@@ -31,6 +34,9 @@ const App = () => {
         <p>good {good}</p>
         <p>neutral {neutral}</p>
         <p>bad {bad}</p>
+        <p>all {all}</p>
+        <p>average {average.toFixed(2)}</p>
+        <p>positive %{percentGood.toFixed(2)}</p>
       </section>
     </div>
   )
