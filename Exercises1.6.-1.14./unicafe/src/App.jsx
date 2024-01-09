9d582d10125e836f0ab2dac5313a0ede532216ce
gitairman <aaronhopkins86@gmail.com>
2024-01-08 19:49:28 -0800

    1.8: unicafe step3 refactor so Statistics is in own component

--- Exercises1.6.-1.14./unicafe/src/App.jsx
+++ Exercises1.6.-1.14./unicafe/src/App.jsx
@@ -1,12 +1,28 @@
 import { useState } from 'react'
 
+const Statistics = ({ stats }) => {
+  const [good, neutral, bad, all, average, percentGood] = stats
+
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average.toFixed(2)}</p>
+      <p>positive %{percentGood.toFixed(2)}</p>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each buttons to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const all = good + neutral + bad
-  const average = (good*1 + neutral*0 + bad*-1) / all || 0
+  const average = (good * 1 + neutral * 0 + bad * -1) / all || 0
   const percentGood = (good / all) * 100 || 0
 
   const handleClickGood = () => {
@@ -29,15 +45,7 @@ const App = () => {
         <button onClick={handleClickNeutral}>neutral</button>
         <button onClick={handleClickBad}>bad</button>
       </section>
-      <section>
-        <h2>Statistics</h2>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>all {all}</p>
-        <p>average {average.toFixed(2)}</p>
-        <p>positive %{percentGood.toFixed(2)}</p>
-      </section>
+      <Statistics stats={[good, neutral, bad, all, average, percentGood]} />
     </div>
   )
 }
