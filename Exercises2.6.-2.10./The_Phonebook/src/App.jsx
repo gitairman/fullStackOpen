01bff932effb3487b9e4a0e06078b701a43fb4d7
gitairman <aaronhopkins86@gmail.com>
2024-01-09 10:15:14 -0800

    2.6: add people to phonebook

---
+++ Exercises2.6.-2.10./The_Phonebook/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (e) => {
+    e.preventDefault()
+    setPersons([...persons, { name: newName }])
+    setNewName('')
+    console.log(persons)
+  }
+
+  const handleChange = (e) => {
+    setNewName(e.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input onChange={handleChange} value={newName} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => <p>{person.name}</p>)}
+    </div>
+  )
+}
+
+export default App
