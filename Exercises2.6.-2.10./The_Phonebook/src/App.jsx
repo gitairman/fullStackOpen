6b6c6f13bad1c861d0af4b7d2884946f04146618
gitairman <aaronhopkins86@gmail.com>
2024-01-09 20:26:39 -0800

    2.12 save new people and numbers to backend server

--- Exercises2.6.-2.10./The_Phonebook/src/App.jsx
+++ Exercises2.6.-2.10./The_Phonebook/src/App.jsx
@@ -9,16 +9,18 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
+  const baseUrl = 'http://localhost:3001/persons'
 
   useEffect(() => {
-    axios.get('http://localhost:3001/persons')
-    .then(res => {
-      setPersons(res.data)
-    })
+    axios.get(baseUrl)
+      .then(res => {
+        setPersons(res.data)
+      })
   }, [])
 
   const addPerson = (e) => {
     e.preventDefault()
+
     if (newName === '' || newNumber === '') {
       alert('Both fields must contain a value!')
     } else if (persons.some(person => person.name === newName)) {
@@ -28,9 +30,14 @@ const App = () => {
       alert(`${newNumber} is already in use by someone else!`)
       setNewNumber('')
     } else {
-      setPersons([...persons, { name: newName, number: newNumber.toString() }])
-      setNewName('')
-      setNewNumber('')
+      const newPerson = { name: newName, number: newNumber.toString() }
+      axios.post(baseUrl, newPerson)
+        .then(res => res.data)
+        .then(returnedPerson => {
+          setPersons([...persons, returnedPerson])
+          setNewName('')
+          setNewNumber('')
+        })
     }
   }
 
@@ -53,7 +60,7 @@ const App = () => {
       <h3>Add a new person</h3>
       <PersonForm elements={[addPerson, handleNameChange, handleNumberChange, newName, newNumber]} />
       <h3>Numbers</h3>
-      <Persons persons={persons} newFilter={newFilter}/>
+      <Persons persons={persons} newFilter={newFilter} />
     </div>
   )
 }
--- Exercises2.6.-2.10./The_Phonebook/src/components/Persons.jsx
+++ Exercises2.6.-2.10./The_Phonebook/src/components/Persons.jsx
@@ -2,8 +2,10 @@ const Persons = ({ persons, newFilter }) => {
     return (
       <>
         {newFilter ?
-          persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase())).map(person => <p key={person.name}>{person.name}: {person.number}</p>) :
-          persons.map(person => <p key={person.name}>{person.name}: {person.number}</p>)
+          persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase()))
+          .map(person => <p key={person.id}>{person.name}: {person.number}</p>) 
+          :
+          persons.map(person => <p key={person.id}>{person.name}: {person.number}</p>)
         }
       </>
     )
