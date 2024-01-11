a4ff818272bde30cba32a8f4e9e895c25bd853b6
gitairman <aaronhopkins86@gmail.com>
2024-01-11 11:09:07 -0800

    2.18: find countries, display only ten in list, display basic data for when only one filtered

--- Exercises2.18.-2.20./countries/src/App.jsx
+++ Exercises2.18.-2.20./countries/src/App.jsx
@@ -1,33 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+let allCountries = []
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api'
+
+  const [filter, setFilter] = useState('')
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    axios.get(`${baseUrl}/all`).then(res => {
+      allCountries = res.data
+    })
+      .catch(err => console.log(err.message))
+
+  }, [])
+
+  useEffect(() => {
+    if (filter !== '') {
+      let filtered = allCountries.filter(country => country.name.common.toLowerCase().includes(filter.toLowerCase()))
+      setCountries(filtered)
+    } else {
+      setCountries([])
+    }
+  }, [filter])
+
+
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value)
+  }
+
+  let dataToRender = []
+
+  if (countries.length > 10) {
+    dataToRender = <p>Too many matches!  Please be more specific.</p>
+  } else if (countries.length === 1) {
+    let key = 0
+    let country = countries[0]
+    dataToRender = [
+        <h2 key={key++}>{country.name.common}</h2>,
+        <p key={key++}>Capital: {country.capital}</p>,
+        <p key={key++}>Population: {country.population.toLocaleString("en-US")}</p>,
+        <p key={key++}>Languages:</p>,
+        <ul key={key++}>
+          {Object.values(country.languages).map(language => <li key={language}>{language}</li>)}
+        </ul>,
+        <p key={key++}>Flag:</p>,
+        <img key={key++} src={country.flags.svg} alt={`${country.name.common}Flag`} width="200" height="100" />
+    ]
+  } else {
+    dataToRender = countries.map(country => <p key={country.cca3}>{country.name.common}</p>)
+  }
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        Find countries: <input type="text" value={filter} onChange={handleFilterChange} />
+        {dataToRender}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
