5fad8da3dbac14f069455e9733291d1cab764425
gitairman <aaronhopkins86@gmail.com>
2024-01-12 09:16:58 -0800

    implement delete by id

--- part03/Exercises3.1.-3.6./phonebook_backend/index.js
+++ part03/Exercises3.1.-3.6./phonebook_backend/index.js
@@ -44,6 +44,17 @@ app.get('/api/persons/:id', (req, res) => {
     }
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const personFound = persons.find(person => person.id === id)
+    if (personFound) {
+        persons = persons.filter(person => person.id !== id)
+        res.status(204).send(`${personFound.name} was successfully deleted`)
+    } else {
+        res.status(404).send(`Person with id ${id} cannot be found`)
+    }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
