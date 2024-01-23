0557def723356f44caeb6221ef9cc4238588bfc4
gitairman <aaronhopkins86@gmail.com>
2024-01-22 20:01:26 -0800

    5.14: test url and likes are shown after button press

--- part05/bloglist-frontend/src/components/Blog.test.js
+++ part05/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders title and author', () => {
@@ -12,7 +13,35 @@ test('renders title and author', () => {
   const { container } = render(<Blog blog={blog} />)
 
   const div = container.querySelector('.blog')
-  
+
   expect(div).toHaveTextContent('this is a test blog')
   expect(div).toHaveTextContent('this is a test author')
+})
+
+test('URL and likes are shown when button controlling the shown details has been clicked', async () => {
+
+  const blog = {
+    title: 'this is a test blog',
+    author: 'this is a test author',
+    url: 'this is a test url',
+    likes: 10,
+    user: {
+      username: 'this is a test username'
+    }
+  }
+
+  const mockHandler = jest.fn()
+
+  const { container } = render(<Blog blog={blog} addedBy={{ username: 'this is a test username' }} />)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+
+  const div = container.querySelector('.blog')
+
+  expect(div).toHaveTextContent('this is a test url')
+  expect(div).toHaveTextContent(10)
+
 })
